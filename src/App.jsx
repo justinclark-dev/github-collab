02554bc0d99619354fe5fed6b418e9ef1ed669a3
Cedric <crubbertire@rogers.com>
2025-11-12 17:41:39 -0500

    Created contional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ import Films from './components/Films/Films';
 import Planets from './components/Planets/Planets'
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("films")
+
+  const handlePageNavigation = async (newPage) => {
+    setCurrentPage(newPage)
+  }
   
   return (
     <>
-      <Nav />
-
-      {/* <Films /> */}
-      <Planets />
+      <Nav handlePageNavigation={handlePageNavigation} />
+      { currentPage === "films" && <Films /> }
+      {/* { currentPage === "people" && <People /> }
+      { currentPage === "species" && <Species /> } */}
+      { currentPage === "planets" && <Planets /> }
     </>
   )
 }
